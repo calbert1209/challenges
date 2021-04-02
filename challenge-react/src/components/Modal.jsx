db495353e5fd2c6ecbc641333fc9373ece5d4703
Christian Albert <calbert1209@gmail.com>
2021-04-02 22:16:45 +0900

    add small screen styling

--- challenge-react/src/components/Modal.jsx
+++ challenge-react/src/components/Modal.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 
-export const ModalScrim = ({ show, onClickScrim, children }) => {
+export const ModalScrim = ({ show, onClickScrim, hasColor, children }) => {
   if (!show) {
     return null;
   }
 
   return createPortal(
-    <div className="modalScrim" onClick={onClickScrim}>
+    <div
+      className="modalScrim"
+      onClick={onClickScrim}
+      data-hasColor={hasColor ?? true}
+    >
       <div onClick={(e) => e.stopPropagation()}>{children}</div>
     </div>,
     document.getElementById('app')
